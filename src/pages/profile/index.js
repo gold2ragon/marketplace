af82270179bc59d98a6127c767eab309f2e6ea8a
Oleg-A <jsprojectcase@gmail.com>
2019-12-25 18:29:41 +0300

    made new branch for profile settings

---
+++ src/pages/profile/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ProfileSettings from './settings';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <ProfileSettings />
+      </div>
+    );
+  }
+}
+
+export default Profile;
