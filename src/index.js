fc8f28d6f06f57f188d67504e34fade82ffd2450
r3dwc <skillful3007@gmail.com>
2019-12-25 18:51:07 +0500

    fix file name of app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
-import App from './app';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
